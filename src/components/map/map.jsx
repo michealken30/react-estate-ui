c7efb01cc7232b957a9f965eaff0cfb3376baba8
Micheal Oshogwe <kenneth.oshogwe@gmail.com>
2024-04-25 07:05:14 +0100

    working on estata UI

---
+++ src/components/map/map.jsx
@@ -0,0 +1,24 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+import "./map.scss";
+import "leaflet/dist/leaflet.css";
+import Pin from "../pin/pin";
+
+const Map = ({ items }) => {
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      className="map"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {items && items.map((item) => <Pin item={item} key={item.id} />)}
+    </MapContainer>
+  );
+};
+
+export default Map;
--- src/routes/ListPage/ListPage.jsx
+++ src/routes/ListPage/ListPage.jsx
@@ -1,5 +1,26 @@
+import { listData } from "/src/lib/dummydata.jsx";
+import "./listpage.scss";
+import Filter from "../../components/filter/filter";
+import Card from "../../components/card/card";
+import Map from "../../components/map/map";
+
 const ListPage = () => {
-  return <div className="listpage">ListPage</div>;
+  const data = listData;
+  return (
+    <div className="listpage">
+      <div className="listcontainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+      <div className="mapcontainer">
+        <Map items={data} />
+      </div>
+    </div>
+  );
 };
 
 export default ListPage;
